46b89bed34c20c19537af4cb4d8ed0413b7c7d76
Jessica Meza <jessicamezarojas2011@gmail.com>
2018-02-13 01:38:35 -0500

    cambiando imagen de bg-home

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 let begin = () => {
-
   // Escritura de letras tipo maquina
   let writing = str => {
     const name = document.getElementById('name');
     let arrFromStr = str.split('');
     let i = 0;
-    let printStr = setInterval(function(){
-      if(arrFromStr[i] === ' '){
+    let printStr = setInterval(function() {
+      if (arrFromStr[i] === ' ') {
         name.innerHTML += arrFromStr[i];
         name.innerHTML += arrFromStr[i + 1];
         i += 2;
@@ -14,22 +13,23 @@ let begin = () => {
         name.innerHTML += arrFromStr[i];
         i++;
       }
-      if(i === arrFromStr.length){
-        clearInterval(printStr)
-        name.style.fontWeight = '600'
+      if (i === arrFromStr.length) {
+        clearInterval(printStr);
+        name.style.color = 'white';
+        name.style.fontWeight = '520';
       }
-    },400);
+    }, 300);
   };
   // Hover de btn seccion de presentación
   let changeArrow = () => {
     const btnArrow = document.getElementById('button-arrow');
-    btnArrow.addEventListener('mouseover', function(){
+    btnArrow.addEventListener('mouseover', function() {
       btnArrow.innerHTML = 'Ver mi Trabajo <i class="material-icons align-middle">arrow_downward</i>';
-    })
-    btnArrow.addEventListener('mouseout', function(){
+    });
+    btnArrow.addEventListener('mouseout', function() {
       btnArrow.innerHTML = 'Ver mi Trabajo <i class="material-icons align-middle">arrow_forward</i>';
-    })
-  }
+    });
+  };
   // Efecto auto scroll- plugin
   let smoothAutoScroll = () => {
     let scroll = new SmoothScroll('a[href*="#"]', {
@@ -38,22 +38,20 @@ let begin = () => {
       speed: 500,
       offset: 0, 
       easing: 'easeInOutCubic', 
-      customEasing: function (time) {
+      customEasing: function(time) {
         return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
       },
     
-      before: function (anchor, toggle) {},
-      after: function (anchor, toggle) {}
+      before: function(anchor, toggle) {},
+      after: function(anchor, toggle) {}
     });
-  }
+  };
 
-  writing('Jessica Meza R.')
+  writing('Jessica Meza Rojas');
   changeArrow();
   smoothAutoScroll();
-
 };
 
 window.addEventListener('load', begin);
-
 
 
